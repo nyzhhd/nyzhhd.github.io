1c0dd3039e55160711ae4e3134453375dc775933
nyzhhd <3268407065@qq.com>
2025-05-20 10:19:43 +0800

    ccdc

---
+++ ccdc2025/js/menu.js
@@ -0,0 +1,43 @@
+var menu = '<nav class="navbar navbar-default mega-nav">'+
+'	  <div class="container-fluid">'+
+'		 <div class="navbar-header">'+
+'			<button type="button" class="pull-left navbar-toggle" data-toggle="collapse" data-target="#myNavbar">'+
+'			  <span class="icon-bar"></span>'+
+'			  <span class="icon-bar"></span>'+
+'			  <span class="icon-bar"></span>                        '+
+'			</button>'+
+'		'+
+'		 </div>'+
+'		 <div class="collapse navbar-collapse" id="myNavbar">'+
+'			<ul class="nav navbar-nav">'+
+'			  <li><a href="../index.html">Home</a></li>'+
+
+'			  <li class="dropdown">'+
+'				 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Proceedings Info<span class="caret"></span></a>'+
+'				 <ul class="dropdown-menu">'+
+'              		<li><a href="welcome.html">Message From General Chairs</a></li>'+
+'              		<li><a href="welcome1.html">Message From Technical Chairs</a></li>'+
+'              		<li><a href="oc.html">Committees</a></li>'+
+'              		<li><a href="keynote.html">Keynote Addresses</a></li>'+
+'              		<li><a href="dislec.html">Distinguished Lectures</a></li>'+
+'              		<li><a href="forms.html">Forums</a></li>'+
+'              		<li><a href="workshops.html">Thematic Workshops</a></li>'+
+'              		<li><a href="specialsessions.html">Special Sessions</a></li>'+
+'				 </ul>'+
+'			  </li>'+
+'			  <li class="dropdown">'+
+'				 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Technical Programme<span class="caret"></span></a>'+
+'				 <ul class="dropdown-menu">'+
+'<li><a href="toc.html#1">Saturday, 17 May 2025</a></li>'+
+'<li><a href="toc.html#2">Sunday, 18 May 2025</a></li>'+
+'<li><a href="toc.html#3">Monday, 19 May 2025</a></li>'+
+'				 </ul>'+
+'			  </li>'+
+'          <li><a href="author_index.html">Author Index</a></li>'+
+'          <li><a href="search_all.html">Search</a></li>'+
+'          <li><a href="acrobat_reader.html">Acrobat Reader</a></li>'+
+'			</ul>'+
+'		 </div>'+
+'	  </div>'+
+'	</nav>';
+
